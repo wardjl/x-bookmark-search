d9577b42506e8651afac55ec66ce85277c08df7f
Ward <wardleenders@gmail.com>
2025-01-12 16:13:37 +0100

    Updated API key

--- extension/config.js
+++ extension/config.js
@@ -1,6 +1,2 @@
-export const GROK_API_KEY = 'xai-ZYdPo0vEHkCd6Hec327j7wEYDBp8h49bCFZOrm1QJtbJbBP94PnuvEj1qOExWyai1TR2jhidoqZXQe4z';
+export const GROK_API_KEY = '';
 export const GROK_MODEL = 'grok-2';
-
-// Remove or comment out OpenAI config if not needed
-// export const OPENAI_API_KEY = '...';
-// export const OPENAI_MODEL = '...'; 
